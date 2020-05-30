e45528c1a47fb026569760e10c6ca67656a58a71
ongspxm <ongspxm@gmail.com>
2020-05-31 07:49:03 +0800

    (comments)

--- gdp.py
+++ gdp.py
@@ -8,12 +8,22 @@ file_gadm = 'data/gadm36_USA_1'
 def main():
     data = {}
     with open(file_data, 'r') as f:
+        # split into different lines and remove first line (the header)
         lines = f.read().split('\n')
         lines.pop(0)
+
+        # process the rest of the lines
         for line in lines:
+            # split by commas
             parts = line.split(',')
-            if parts[0]:
-                data[parts[0].strip()] = sum([float(i) for i in parts[1:]])
+
+            # clean up the name, and convert rest to float
+            name = parts[0].strip()
+            rest = [float(i) for i parts[1:]]
+
+            # set the val to the sum of all the quarters
+            if name:
+                data[name] = sum(rest)
 
     heatmap.generateShapes(file_gadm, data, 'gdp.tif', np.float32)
 
