55f5f1ac0c0327989a52e11917d3b3fa8e550676
ongspxm <ongspxm@gmail.com>
2020-05-30 14:59:05 +0800

    hotspot filled

--- script.py
+++ script.py
@@ -1,21 +1,73 @@
-import utils
+""" whole project in int, cos easier """
 
+import utils
 import json
+
+import rasterio
 import shapefile
+import numpy as np
 
 file_covid = 'covid.json'
 file_gadm = 'data/gadm36_USA_0'
 
+Affine = rasterio.transform.Affine
+
 def echo(msg):
     print('[log]', msg)
 
-with open(file_covid, 'r') as f:
-    data = json.load(f)
-features = data['features']
+def getActive():
+    # convert to mdeg
+    with shapefile.Reader(file_gadm) as sf:
+        pts, bnds0, bnds1 = utils.get_bounds2cell(sf.shape(0).points)
+        echo('reading done')
 
-# convert to mdeg
-sf = shapefile.Reader(file_gadm)
-pts, bnds0, bnds1 = utils.get_bounds2cell(sf.shape(0).points)
-echo('reading done')
+    active = utils.mark_cells(pts, bnds0)
+    return active, bnds0, bnds1
 
-utils.mark_cells(pts, bnds0)
+def getFeatures():
+    with open(file_covid, 'r') as f:
+        data = json.load(f)
+    return data['features']
+
+def main():
+
+    cells, lons, lats = getActive()
+    def cell2rc(coords):
+        return (coords[0]-lons[0], coords[1]-lats[0])
+
+    grid = np.zeros((lons[1]-lons[0], lats[1]-lats[0], 1), np.uint32)
+    transform = Affine.scale(utils.RES)
+    transform *= Affine.translation(lons[0],lats[0])
+    echo('initialization done')
+
+    # fill in the hotspot map
+    feats = getFeatures()
+    for feat in feats:
+        attrs = feat['attributes']
+        lat, lon = attrs['Lat'], attrs['Long_']
+        if not lon or not lat: continue
+
+        confirmed, recovered =  attrs['Confirmed'], attrs['Recovered']
+        deaths, active = attrs['Deaths'], attrs['Active']
+        cell = utils.deg2cell((lon,lat))
+
+        if cell in cells:
+            utils.fill_hotspot(grid, cell2rc(cell), confirmed)
+    echo('grid filled')
+
+    # write to tiff file
+    tiff = rasterio.open(
+         'test.tif', 'w',
+         driver='GTiff',
+         height=grid.shape[0],
+         width=grid.shape[1],
+         count=1, dtype=grid.dtype,
+         crs='+proj=latlong',
+         transform=transform,
+    )
+    tiff.write(grid[:,:,0], 1)
+    tiff.close()
+    echo('tif file saved')
+
+if __name__ == '__main__':
+    main()
--- utils.py
+++ utils.py
@@ -1,8 +1,13 @@
+import math
+
 # resolution of tif file (mdeg)
 RES = 1000
 
+# when to stop the algo
+HEATMAP_CUTOFF = 1
+
 def deg2cell(coord):
-    return [int(i*1000/RES) for i in coord]
+    return tuple([int(i*1000/RES) for i in coord])
 
 def get_bounds2cell(points):
     pts = []
@@ -19,7 +24,9 @@ def get_bounds2cell(points):
         if len(pts) and pts[-1]==(a,b):
             continue
         pts.append((a,b))
-    return pts, mins, maxs
+
+    lons, lats = [(mins[i], maxs[i]) for i in range(2)]
+    return pts, lons, lats
 
 def mark_cells(pts, lons):
     """ polygon fill algo """
@@ -46,5 +53,26 @@ def mark_cells(pts, lons):
             cells.append((lon, b))
             if i%2:
                 cells += [(lon, j) for j in range(a+1,b+1)]
-    cells = set(cells)
-    print(len(cells))
+    return set(cells)
+
+def fn_inv_deg_sq(start, targ, val):
+    if start==targ: return val
+
+    # dist in mdeg
+    dist = math.sqrt(sum([((start[i]-targ[i])*RES)**2 for i in range(2)]))/1000
+    return val/(dist**2)
+
+def lim_inv_deg_sq(val):
+    return HEATMAP_CUTOFF*math.sqrt(val)
+
+def fill_hotspot(grid, start, val):
+    W, H, _ = grid.shape
+    diff = lim_inv_deg_sq(val)
+
+    # end is exclusive
+    minr, maxr = int(max(start[0]-diff, 0)), int(min(start[0]+diff+1, W))
+    minc, maxc = int(max(start[1]-diff, 0)), int(min(start[1]+diff+1, H))
+
+    for r in range(minr, maxr):
+        for c in range(minc, maxc):
+            grid[r][c] += int(fn_inv_deg_sq(start, (r,c), val))
